709812c0f2012d48345a346e3f37fd08fdaa3fa3
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-04-04 09:10:52 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Lewis Brocklehurst on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
